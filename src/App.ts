bfda4e2ed3282723433d6bacc1ef7c3941664593
Ashikur Rahman <ashikur.rahman9997@gmail.com>
2023-06-28 00:54:25 +0600

    test

--- src/App.ts
+++ src/App.ts
@@ -10,25 +10,30 @@ app.use(cors());
 
 app.use(express.json());
 
-
-
-
-
-
-
-
-
-
 app.use(express.urlencoded({ extended: true }));
 
-// application route
-app.use('/api/v1/', router);
 
+
+
+
+
+
+
+
+
+// application route
+app.se('/api/v1/', router);
 
 // global error handler
 app.use(globalErrorHandler);
 
 // not found route
+
+
+
+
+
+
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
